87db83e915057ae7eef3af5ce5b5b760159677fc
tkdgusqkr <sanghyean1@hanyang.ac.kr>
2026-06-09 08:02:23 +0900

    feat: replace boolean trainingCompleted with 3-way TestTrainingStatus enum (온라인/오프라인/미완료)

--- development/service-ops-api/src/main/java/com/thundercrew/opsapi/testmatching/rider/dto/TestRiderCreateRequest.java
+++ development/service-ops-api/src/main/java/com/thundercrew/opsapi/testmatching/rider/dto/TestRiderCreateRequest.java
@@ -1,6 +1,7 @@
 package com.thundercrew.opsapi.testmatching.rider.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.thundercrew.opsapi.testmatching.rider.domain.TestTrainingStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -10,6 +11,6 @@ import jakarta.validation.constraints.Size;
 public record TestRiderCreateRequest(
         @NotBlank @Size(max = 100) String name,
         @NotBlank @Pattern(regexp = "010-\\d{4}-\\d{4}", message = "연락처 형식: 010-XXXX-XXXX") String phoneNumber,
-        @NotNull Boolean trainingCompleted,
+        @NotNull TestTrainingStatus trainingStatus,
         @Size(max = 100) String teamName
 ) {}
